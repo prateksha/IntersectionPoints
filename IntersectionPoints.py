5b42ae0273986ffa997d2ae354e244d8595c27d8
Ayush Yadav <ayushyadav.ay42@gmail.com>
2019-04-23 21:41:34 +0530

    add file input

--- IntersectionPoints.py
+++ IntersectionPoints.py
@@ -1,19 +1,68 @@
-number_of_lines = input()
-# array_of_variables = [[0,0],[0,0],[0,0],[0,0],[0,0]]
-array_of_variables = [[0 for x in range(2)] for y in range(number_of_lines)] 
-for i in range(number_of_lines):
-    m = input()
-    c = input()
-    array_of_variables[i][0] = m
-    array_of_variables[i][1] = c
+def mergeSort(array, sortedArray, left, right):
+    mid = (left + right)/2
+    inversions = 0
+    if (right >left):
+        inversions = mergeSort(array, sortedArray, left, mid)
+        inversions += mergeSort(array, sortedArray, mid+1, right)
+        inversions += merge(array, sortedArray, left, right, mid+1)
+    return inversions
 
-print(array_of_variables)
+
+
+def merge(array, sortedArray, left, right, mid):
+    inversions = 0
+    i = left
+    j = mid
+    k = left
+    while (i < mid  and j <= right):
+        if (array[i] <= array[j]):
+            sortedArray[k] = array[i]
+            i += 1
+            k += 1
+        else :
+            sortedArray[k] = array[j]
+            inversions += (mid - i)
+            j += 1
+            k += 1 
+    while (i < mid):
+        sortedArray[k] = array[i]
+        i += 1
+        k += 1
+    while (j <= right):
+        sortedArray[k] = array[j]
+        j += 1
+        k += 1 
+    i = left
+    while(i <= right):
+        array[i] = sortedArray[i]
+        i+=1
+
+    return inversions
+
+
+array_of_variables = []
+fname = 'input.txt'
+with open(fname, 'r') as f:
+    no_of_lines = int(f.readline())
+    for i in range(no_of_lines):
+        m, c = map(int, f.readline().split())
+        array_of_variables.append([m, c])
+    print array_of_variables
+
 # Sorting based on c
 array_of_variables.sort(key=lambda p:p[1])
 print(array_of_variables)
 
-slopes_array = [0 for x in range(number_of_lines)]
-for i in range(number_of_lines):
+slopes_array = [0 for x in range(no_of_lines)]
+for i in range(no_of_lines):
     slopes_array[i] = array_of_variables[i][0]
 print(slopes_array)
 
+sorted_array = []
+for i in range(len(slopes_array)):
+    sorted_array.append(0)
+print (mergeSort(slopes_array, sorted_array, 0, len(slopes_array)-1))
+
+
+
+	
